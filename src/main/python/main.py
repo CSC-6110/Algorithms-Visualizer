0bf596ce76ae1e6b8a61e850998cfd6b4c47ca20
Abhishek <abhi.ashwin@gmail.com>
2021-11-19 09:53:24 -0500

    linting changes to main.py

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,6 +1,10 @@
-import pygame
+'''
+Docstring
+'''
+
 from random import randint
 from time import time
+import pygame
 from algs import algorithmsDict
 import display
 
@@ -10,21 +14,24 @@ import display
 
 
 def main():
+    '''
+    main doc string
+    '''
     numbers = []
     running = True
     display.algorithmBox.add_options(list(algorithmsDict.keys()))
 
-    current_alg = None
-    alg_iterator = None
+    currentAlg = None
+    algIterator = None
 
-    timer_delay = time()
-    
+    timerDelay = time()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and display.do_sorting:
+            elif event.type ==pygame.KEYDOWN and event.key == pygame.K_SPACE and display.do_sorting:
                 display.paused = not display.paused
                 display.timer_space_bar = time()
 
@@ -35,10 +42,11 @@ def main():
         if display.playButton.isActive: # play button clicked
             display.playButton.isActive = False
             display.do_sorting = True
-            current_alg = display.algorithmBox.get_active_option()
+            currentAlg = display.algorithmBox.get_active_option()
             display.numBars = int(display.sizeBox.text)
             numbers = [randint(10, 400) for i in range(display.numBars)] # random list to be sorted
-            alg_iterator = algorithmsDict[current_alg](numbers, 0, display.numBars-1) # initialize iterator
+            # initialize iterator
+            algIterator = algorithmsDict[currentAlg](numbers, 0, display.numBars-1)
 
         if display.stopButton.isActive: # stop button clicked
             display.stopButton.isActive = False
@@ -46,23 +54,23 @@ def main():
             display.paused = False
             try: # deplete generator to display sorted numbers
                 while True:
-                    numbers, redBar1, redBar2, blueBar1, blueBar2 = next(alg_iterator)
+                    numbers, redBar1, redBar2, blueBar1, blueBar2 = next(algIterator)
             except StopIteration:
                 pass
 
         if display.do_sorting and not display.paused: # sorting animation
             try:
-                if time()-timer_delay >= display.delay:
-                    numbers, redBar1, redBar2, blueBar1, blueBar2 = next(alg_iterator)
+                if time()-timerDelay >= display.delay:
+                    numbers, redBar1, redBar2, blueBar1, blueBar2 = next(algIterator)
                     display.drawInterface(numbers, redBar1, redBar2, blueBar1, blueBar2)
-                    timer_delay = time()
+                    timerDelay = time()
             except StopIteration:
                 display.do_sorting = False
         elif display.do_sorting and display.paused: # animation paused
             display.drawInterface(numbers, -1, -1, -1, -1)
         else: # no animation
-            a_set = set(range(display.numBars))
-            display.drawInterface(numbers, -1, -1, -1, -1, greenRows=a_set)
+            aSet = set(range(display.numBars))
+            display.drawInterface(numbers, -1, -1, -1, -1, greenRows=aSet)
 
 if __name__ == '__main__':
     main()
